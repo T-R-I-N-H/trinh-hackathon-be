df675c05c20f2bf8fb08e1ee4086f20d9c55e704
Phan Trung Dũng <aninatsumi@gmail.com>
2025-06-05 22:40:20 +0700

    init project

---
+++ src/main/java/trinh_hackathon_be/trinh/TrinhHackathonBeApplication.java
@@ -0,0 +1,13 @@
+package trinh_hackathon_be.trinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinhHackathonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrinhHackathonBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trinh_hackathon_be/trinh/TrinhHackathonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package trinh_hackathon_be.trinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrinhHackathonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
